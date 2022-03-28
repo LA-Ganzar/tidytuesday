bfcdf90cc5d2dd040d4e147e0d22c9127405a4fa
Leigh Ann Ganzar, DrPH MPH <88208717+LA-Ganzar@users.noreply.github.com>
2022-03-28 14:49:09 -0500

    add week 13

--- 2022/2022-03-29/TT_20220329.R
+++ 2022/2022-03-29/TT_20220329.R
@@ -1,1 +1,10 @@
+# Tidy Tuesday Week 13
+# 2022-03-29
+# The purpose of this week's project is to
 
+# Libraries
+library(tidyverse)
+library(tidytuesdayR)
+
+# Reading data and wrangling
+tuesdata <- tidytuesdayR::tt_load(2022, week = 13)
